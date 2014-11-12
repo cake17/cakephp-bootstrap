1f6db27fafc0f45d2e6faa5151d36c3517faed55
cake17 <cake17@cake-websites.com>
2014-11-12 13:05:02 +0100

    reorganize form templates

--- config/bs_form_inline.php
+++ config/bs_form_inline.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * configs for inline form
- * 
+ *
  * @author   cake17
  * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
  * @link     http://cake17.github.io/
@@ -9,9 +9,9 @@
  */
 $config = [
 	'button' => '<button{{attrs}} class="btn btn-default btn-md">{{text}}</button>',
-	'checkbox' => '<input type="checkbox" name="{{name}}" value="{{value}}"{{attrs}}> {{name}}',
-	'checkboxFormGroup' => '{{input}}{{label}}',
-	'checkboxWrapper' => '<div class="checkbox">{{input}}{{label}}</div>',
+	'checkbox' => '<input type="checkbox" name="{{name}}" value="{{value}}"{{attrs}}>',
+	'checkboxFormGroup' => '{{label}}',
+	'checkboxWrapper' => '<div class="checkbox">{{label}}</div>',
 	'dateWidget' => '{{year}}{{month}}{{day}}{{hour}}{{minute}}{{second}}{{meridian}}',
 	'error' => '<div class="error-message">{{content}}</div>',
 	'errorList' => '<ul>{{content}}</ul>',
@@ -25,15 +25,16 @@ $config = [
 	'input' => '<input class="form-control" type="{{type}}" name="{{name}}"{{attrs}}>',
 	'inputsubmit' => '<input type="{{type}}"{{attrs}}>',
 	'inputContainer' => '<div class="form-group input {{type}}{{required}}">{{content}}</div>',
-	'inputContainerError' => '<div class="form-group input {{type}}{{required}} has-error has-feedback">{{content}}{{error}}<span class="glyphicon glyphicon-remove form-control-feedback"></span></div>',
+	'inputContainerError' => '<div class="form-group input {{type}}{{required}} has-error has-feedback">{{content}}{{error}}<span class="glyphicon glyphicon-remove form-control-feedback"></div>',
 	'label' => '<label{{attrs}} class="sr-only">{{text}}</label>',
+	'nestingLabel' => '{{hidden}}<label{{attrs}}>{{input}}{{text}}</label>',
 	'legend' => '<legend>{{text}}</legend>',
 	'option' => '<option value="{{value}}"{{attrs}}>{{text}}</option>',
 	'optgroup' => '<optgroup label="{{label}}"{{attrs}}>{{content}}</optgroup>',
 	'select' => '<select class="form-control" name="{{name}}"{{attrs}}>{{content}}</select>',
 	'selectMultiple' => '<select name="{{name}}[]" multiple="multiple"{{attrs}}>{{content}}</select>',
 	'radio' => '<input type="radio" name="{{name}}" value="{{value}}"{{attrs}}>',
-	'radioWrapper' => '{{input}}{{label}}',
+	'radioWrapper' => '{{label}}',
 	'textarea' => '<textarea class="form-control" name="{{name}}"{{attrs}}>{{value}}</textarea>',
-	'submitContainer' => '<div class="submit">{{content}}</div>',
+	'submitContainer' => '<div class="submit">{{content}}</div>'
 ];
