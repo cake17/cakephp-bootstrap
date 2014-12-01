14ac53fe763ced787a6cd9fcd89ef8c167823aba
cake17 <cake17@cake-websites.com>
2014-12-01 18:18:28 +0100

    remove bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,47 +16,19 @@ require ROOT . '/vendor/cakephp/cakephp/src/basics.php';
 require ROOT . '/vendor/autoload.php';
 Cake\Core\Configure::write('App', [
 	'namespace' => 'App',
-	'encoding' => 'UTF-8']);
+	'encoding' => 'UTF-8'
+]);
 Cake\Core\Configure::write('debug', true);
 mb_internal_encoding('UTF-8');
-$Tmp = new \Cake\Filesystem\Folder(TMP);
-$Tmp->create(TMP . 'cache/models', 0770);
-$Tmp->create(TMP . 'cache/persistent', 0770);
-$Tmp->create(TMP . 'cache/views', 0770);
-$cache = [
-	'default' => [
-		'engine' => 'File'
-	],
-	'_cake_core_' => [
-		'className' => 'File',
-		'prefix' => 'crud_myapp_cake_core_',
-		'path' => CACHE . 'persistent/',
-		'serialize' => true,
-		'duration' => '+10 seconds'
-	],
-	'_cake_model_' => [
-		'className' => 'File',
-		'prefix' => 'crud_my_app_cake_model_',
-		'path' => CACHE . 'models/',
-		'serialize' => 'File',
-		'duration' => '+10 seconds'
-	]
-];
-Cake\Cache\Cache::config($cache);
-Cake\Core\Plugin::load('Tools', ['path' => './']);
-// Ensure default test connection is defined
-if (!getenv('db_class')) {
-	putenv('db_class=Cake\Database\Driver\Sqlite');
-	putenv('db_dsn=sqlite::memory:');
-}
-Cake\Datasource\ConnectionManager::config('test', [
-	'className' => 'Cake\Database\Connection',
-	'driver' => getenv('db_class'),
-	'dsn' => getenv('db_dsn'),
-	'database' => getenv('db_database'),
-	'login' => getenv('db_login'),
-	'password' => getenv('db_password'),
-	'timezone' => 'UTC',
-	'quoteIdentifiers' => true,
-	'cacheMetadata' => true,
-]);
+
+// Cake\Datasource\ConnectionManager::config('test', [
+// 	'className' => 'Cake\Database\Connection',
+// 	'driver' => getenv('db_class'),
+// 	'dsn' => getenv('db_dsn'),
+// 	'database' => getenv('db_database'),
+// 	'login' => getenv('db_login'),
+// 	'password' => getenv('db_password'),
+// 	'timezone' => 'UTC',
+// 	'quoteIdentifiers' => true,
+// 	'cacheMetadata' => true,
+// ]);
